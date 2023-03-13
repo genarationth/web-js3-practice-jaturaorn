2b1214cd976a2f2359c319f90f2123bee8ed2565
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-13 09:28:12 +0000

    fix fucsub, add divide

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const add = (a,b) => {
 }
 console.log(add(20,10));
 
-const subtract = (a,b) => a/b;
+const subtract = (a,b) => a-b;
 console.log(subtract(40,10));
 
 const multiply = (a,b) => a*b;
 console.log(multiply(2,300));
 
+const divide = (a,b) => a/b;
+console.log(divide(400,2));
